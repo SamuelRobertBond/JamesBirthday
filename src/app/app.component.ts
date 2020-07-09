75a788f272dbc955f590c3e50c4c6181a53ec6f8
Sam Bond <foamyphyco@gmail.com>
2020-07-08 20:51:15 -0400

    birthday site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'jamesbirthdaysite';
+export class AppComponent implements OnInit {
+
+  jamesBirthday = 'jamesbirthdaysite';
+
+  ngOnInit() {
+    this.setBirthday();
+    setInterval(() => {
+      this.setBirthday()
+    }, 1000)
+  }
+
+  setBirthday() {
+    let tomorrow = new Date()
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    this.jamesBirthday = tomorrow.toLocaleDateString("en", {
+      weekday: "short",
+      year: "numeric",
+      month: "2-digit",
+      day: "numeric"
+    })
+  }
+
 }
